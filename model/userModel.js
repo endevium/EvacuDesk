ecfba70489b655c1dd9a61d8ce633735b201d4fc
Oliver <smitholiver106@gmail.com>
2025-09-27 22:21:28 +0800

    initial commit

--- model/userModel.js
+++ model/userModel.js
@@ -3,9 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     last_name: { type: String, required: true },
     first_name: { type: String, required: true },
+    address: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['Evacuess', 'user'], default: 'user' }
+    role: { type: String, enum: ['Evacuee', 'Staff', 'Admin'], default: 'Evacuee' }
 }, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);

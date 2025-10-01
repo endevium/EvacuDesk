d8c74aaf5c2522ae01c7227cd666044f9b12be35
John Rasheed Paner <148771728+endevium@users.noreply.github.com>
2025-10-01 13:51:52 +0800

    Delete routes directory

--- routes/AdminRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const AdminController = require("../controllers/AdminController");
-
-router.post("/login", AdminController.loginAdmin);
-// router.patch("/:id", AdminController.updateAdmin);
-router.patch("/password/:id", AdminController.updatePassword);
-
-module.exports = router;
